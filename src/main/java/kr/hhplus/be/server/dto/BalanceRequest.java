5db1a20b4d4ba8042d5dd23fb18bec39b063f753
ksh <googoojoa@gmail.com>
2025-07-17 23:06:42 +0900

    feat : BalanceRequest.java 수정

--- src/main/java/kr/hhplus/be/server/dto/BalanceRequest.java
+++ src/main/java/kr/hhplus/be/server/dto/BalanceRequest.java
@@ -1,4 +1,20 @@
 package kr.hhplus.be.server.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class BalanceRequest {
+    @Schema(description = "유저 ID", example = "1")
+    private Long userId;
+
+    @Schema(description = "충전 금액", example = "1000")
+    private Long amount;
+
+    // 생성자, getter, setter
+    public BalanceRequest() {}
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public Long getAmount() { return amount; }
+    public void setAmount(Long amount) { this.amount = amount; }
 }
