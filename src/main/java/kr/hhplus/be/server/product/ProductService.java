c77163b2836b15101ae5d651e347a7550aeaf097
ksh <googoojoa@gmail.com>
2025-07-22 17:28:03 +0900

    feat: ProductService 상품 상세 조회 메서드 추가

--- src/main/java/kr/hhplus/be/server/product/ProductService.java
+++ src/main/java/kr/hhplus/be/server/product/ProductService.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.product;
 
+import kr.hhplus.be.server.common.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,12 @@ public class ProductService {
         return  products.stream()
                 .map(product -> new ProductResponse(product))
                 .collect(Collectors.toList());
+    }
+
+    // 상품 상세 조회
+    public ProductDetailResponse getProductById(Long productId){
+        Product product = productRepository.findById(productId)
+                .orElseThrow(()-> new EntityNotFoundException("상품을 찾을 수 없습니다."));
+        return new ProductDetailResponse(product);
     }
 }
