54d8b5a3e8b55536006e34a5e803049b09be18c7
ksh <googoojoa@gmail.com>
2025-07-22 20:49:19 +0900

    feat: UserService 포인트 사용 및 포인트 이력 저장 메서드 추가

--- src/main/java/kr/hhplus/be/server/user/UserService.java
+++ src/main/java/kr/hhplus/be/server/user/UserService.java
@@ -32,5 +32,21 @@ public class UserService {
         pointHistoryRepository.save(history);
     }
 
+    // 포인트 사용
+    @Transactional
+    public void usePoint(Long userId, int amount){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다. id=" + userId));
 
+        user.usePoint(amount);
+
+        // 포인트 이력 저장
+        PointHistory history = new PointHistory();
+        history.setUserId(userId);
+        history.setAmount(-amount);
+        history.setPointType("USE");
+        history.setCreatedAt(LocalDateTime.now());
+
+        pointHistoryRepository.save(history);
+    }
 }
