91d75b488d66f1ef44d16f61238a041f26ec0205
ksh <googoojoa@gmail.com>
2025-07-22 16:16:36 +0900

    feat: ProductResponse DTO 클래스 생성

--- src/main/java/kr/hhplus/be/server/product/ProductResponse.java
+++ src/main/java/kr/hhplus/be/server/product/ProductResponse.java
@@ -1,4 +1,22 @@
 package kr.hhplus.be.server.product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProductResponse {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer stockQuantity;
+
+    // Product 엔티티로부터 DTO 생성
+    public ProductResponse(Product product) {
+        this.id = product.getProductId();
+        this.name = product.getProductName();
+        this.price = product.getProductPrice();
+        this.stockQuantity = product.getStockQty();
+    }
 }
