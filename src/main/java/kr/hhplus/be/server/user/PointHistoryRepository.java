5d31294000a84b41fdd9cdacb38b88cdf23a7c05
ksh <googoojoa@gmail.com>
2025-07-22 20:37:16 +0900

    feat: UserRepository, PointHistoryRepository JpaRepository 기반으로 생성

--- src/main/java/kr/hhplus/be/server/user/PointHistoryRepository.java
+++ src/main/java/kr/hhplus/be/server/user/PointHistoryRepository.java
@@ -2,5 +2,5 @@ package kr.hhplus.be.server.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 }
