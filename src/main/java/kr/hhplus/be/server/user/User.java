626e3f2f416fec2fcd98fe2f21ecf6ef63426f12
ksh <googoojoa@gmail.com>
2025-07-22 19:59:40 +0900

    feat: User에 포인트 충전 메서드 추가

--- src/main/java/kr/hhplus/be/server/user/User.java
+++ src/main/java/kr/hhplus/be/server/user/User.java
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.user;
 
 import jakarta.persistence.*;
+import kr.hhplus.be.server.common.exception.InsufficientBalanceException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,11 +13,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long id;
+    private Long userId;
 
     @Column(name = "point_balance", nullable = false)
     private Integer pointBalance;
 
     @Column(name = "user_grade", nullable = false)
     private String userGrade = "NORMAL";
+
+    // 포인트 충전
+    public void chargePoint(int amount){
+        this.pointBalance += amount;
+    }
+
 }
