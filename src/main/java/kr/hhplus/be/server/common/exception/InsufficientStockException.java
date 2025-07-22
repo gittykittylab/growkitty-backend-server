f3ece2f8d3cb83f7f0c3b883b449757e06cc9c72
ksh <googoojoa@gmail.com>
2025-07-22 15:57:09 +0900

    feat: 재고부족 예외 처리 메서드 추가

--- src/main/java/kr/hhplus/be/server/common/exception/InsufficientStockException.java
+++ src/main/java/kr/hhplus/be/server/common/exception/InsufficientStockException.java
@@ -1,4 +1,19 @@
 package kr.hhplus.be.server.common.exception;
 
-public class InsufficientStockException {
+public class InsufficientStockException extends RuntimeException {
+
+    // 기본 생성자
+    public InsufficientStockException() {
+        super("재고가 부족합니다");
+    }
+
+    // 메시지를 받는 생성자
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+
+    // 메시지와 원인 예외를 받는 생성자
+    public InsufficientStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
