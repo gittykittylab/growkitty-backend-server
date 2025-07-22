ff54546c5dd917dfdf9bcc2c1d884e82bf2af4e0
ksh <googoojoa@gmail.com>
2025-07-22 16:18:02 +0900

    feat: ProductService에 상품 목록 조회 메서드 추가

--- src/main/java/kr/hhplus/be/server/product/ProductService.java
+++ src/main/java/kr/hhplus/be/server/product/ProductService.java
@@ -1,4 +1,23 @@
 package kr.hhplus.be.server.product;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    //상품 목록 조회
+    public List<ProductResponse> getProducts(){
+        List<Product> products = productRepository.findAll();
+        return  products.stream()
+                .map(product -> new ProductResponse(product))
+                .collect(Collectors.toList());
+    }
 }
