564b31fafb58978523a19462df912abf9b98e5e3
ksh <googoojoa@gmail.com>
2025-07-23 09:37:53 +0900

    feat: Order 주문 생성 메서드 추가

--- src/main/java/kr/hhplus/be/server/order/Order.java
+++ src/main/java/kr/hhplus/be/server/order/Order.java
@@ -44,4 +44,25 @@ public class Order {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
-}
+    // 주문 생성
+    public static Order createOrder(Long userId, List<OrderItem> orderItems) {
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setOrderStatus("PENDING");
+        order.setOrderType("NORMAL"); // "NORMAL" 일반 주문 "GIFT" 선물 주문
+        order.setOrderedDt(LocalDateTime.now());
+
+        int totalAmount = 0;
+
+        for (OrderItem orderItem : orderItems) { 
+            order.addOrderItem(orderItem); // 연관관계 설정
+            totalAmount += orderItem.getOrderPrice() * orderItem.getOrderQty(); // 총 금액
+        }
+
+        order.setTotalAmount(totalAmount);
+        order.setTotalDiscountAmount(0);  // 할인 없음
+
+        return order;
+    }
+
+    }
