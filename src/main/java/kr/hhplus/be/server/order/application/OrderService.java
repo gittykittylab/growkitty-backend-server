3cba761d958fc49490af2944b8e1bf76f1ceeb40
ksh <googoojoa@gmail.com>
2025-07-23 15:29:31 +0900

    refactor: OrderService 주문 생성 메서드 추가

--- src/main/java/kr/hhplus/be/server/order/application/OrderService.java
+++ src/main/java/kr/hhplus/be/server/order/application/OrderService.java
@@ -2,10 +2,13 @@ package kr.hhplus.be.server.order.application;
 
 import kr.hhplus.be.server.common.exception.EntityNotFoundException;
 import kr.hhplus.be.server.order.domain.Order;
+import kr.hhplus.be.server.order.domain.OrderItem;
 import kr.hhplus.be.server.order.infrastructure.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,4 +21,13 @@ public class OrderService {
         return orderRepository.findById(orderId)
                 .orElseThrow(() -> new EntityNotFoundException("주문을 찾을 수 없습니다. id=" + orderId));
     }
+
+    // 주문 생성
+    @Transactional
+    public Order createOrder(Long userId, List<OrderItem> orderItems){
+        // Order 엔티티 메서드 활용
+        Order order = Order.createOrder(userId, orderItems);
+        return  orderRepository.save(order);
+    }
+
 }
