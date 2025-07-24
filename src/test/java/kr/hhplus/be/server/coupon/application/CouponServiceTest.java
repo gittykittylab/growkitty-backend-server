29e2513bad76efda238322da5c7e6855fded8d6a
ksh <googoojoa@gmail.com>
2025-07-24 22:34:14 +0900

    test(coupon): 쿠폰 발급 성공 테스트

---
+++ src/test/java/kr/hhplus/be/server/coupon/application/CouponServiceTest.java
@@ -0,0 +1,75 @@
+package kr.hhplus.be.server.coupon.application;
+
+import kr.hhplus.be.server.coupon.domain.Coupon;
+import kr.hhplus.be.server.coupon.domain.CouponPolicy;
+import kr.hhplus.be.server.coupon.infrastructure.CouponPolicyRepository;
+import kr.hhplus.be.server.coupon.infrastructure.CouponRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CouponServiceTest {
+
+    @Mock
+    private CouponPolicyRepository couponPolicyRepository;
+
+    @Mock
+    private CouponRepository couponRepository;
+
+    @InjectMocks
+    private CouponService couponService;
+
+    private Long policyId;
+    private Long userId;
+    private CouponPolicy policy;
+    private Coupon expectedCoupon;
+
+    @BeforeEach
+    void setUp() {
+        // 공통 테스트 데이터 초기화
+        policyId = 1L;
+        userId = 1L;
+
+        policy = new CouponPolicy();
+        policy.setPolicyId(policyId);
+        policy.setTotalQuantity(100);
+
+        expectedCoupon = new Coupon();
+        expectedCoupon.setCouponId(1L);
+
+        policy.setExpiredDays(30); // expiredDays 값 설정 추가
+        policy.setDiscountRate(10);
+        policy.setDiscountAmount(1000);
+
+        // 기본 모킹 설정
+        when(couponPolicyRepository.findById(policyId)).thenReturn(Optional.of(policy));
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 성공")
+    void coupon_issued_success() {
+        // given
+        when(couponRepository.countByPolicyId(policyId)).thenReturn(0L);
+        when(couponRepository.existsByUserIdAndPolicyId(userId, policyId)).thenReturn(false);
+        when(couponRepository.save(any(Coupon.class))).thenReturn(expectedCoupon);
+
+        // when
+        Coupon result = couponService.issueFirstComeCoupon(policyId, userId);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.getCouponId()).isEqualTo(expectedCoupon.getCouponId());
+    }
+}
