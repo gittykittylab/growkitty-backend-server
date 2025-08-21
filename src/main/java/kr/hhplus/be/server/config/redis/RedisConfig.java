ee318f83818abb6fe917d845e940820753f53f9e
ksh <googoojoa@gmail.com>
2025-08-21 17:53:56 +0900

    feat(coupon): lua script 를 위한 Bean 추가

--- src/main/java/kr/hhplus/be/server/config/redis/RedisConfig.java
+++ src/main/java/kr/hhplus/be/server/config/redis/RedisConfig.java
@@ -2,10 +2,15 @@ package kr.hhplus.be.server.config.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
 
 @Configuration
 public class RedisConfig {
@@ -20,5 +25,16 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    /**
+     * 쿠폰 발급 Lua 스크립트
+     */
+    @Bean
+    public RedisScript<List> couponIssueScript() {
+        DefaultRedisScript<List> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource("scripts/coupon_issue.lua"));
+        script.setResultType(List.class);  // 여기는 원시 타입 사용
+        return script;
     }
 }
